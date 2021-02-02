91d05d76e3faab6ce03f434f4a358bd76361c239
Alexander <mybig8@hotmail.com>
2021-02-02 19:54:30 +0500

    Исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
-const getNumbers = (min, max) => {
+const getRandomInt = (min, max) => {
   if (min >= max) {
-    return 'Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального';
-  }  else  {
-    if (min >= 0 && max >= 0) {
-      return Math.trunc(Math.random() * (max - min + 1) + min);
-    }
-    return'Ошибка. Пожалуйста, введите число от 0 и больше';
+    console.log('Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального');
+    return -1;
   }
+  if (min >= 0 && max >= 0) {
+    return Math.trunc(Math.random() * (max - min + 1) + min);
+  }
+  console.log('Ошибка. Пожалуйста, введите число от 0 и больше');
+  return -1;
 }
 
-getNumbers(2, 8);
+getRandomInt(2, 8);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-const getCoordinates = (firstCoordinate, secondCoordinate, lengthCoorinates) => {
-  if (firstCoordinate >= secondCoordinate) {
-    return 'Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального';
-  }  else {
-    if (firstCoordinate >= 0 && secondCoordinate >= 0) {
-      return Number((Math.random() * (secondCoordinate - firstCoordinate + 1) + firstCoordinate).toFixed(lengthCoorinates));
-    }
-    return 'Ошибка. Пожалуйста, введите число от 0 и больше';
+const getRandomFloat = (minNumber, maxNumber, numbersAfterComma) => {
+  if (minNumber >= maxNumber) {
+    console.log('Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального');
+    return -1;
   }
+  if (minNumber >= 0 && maxNumber >= 0) {
+    return Number((Math.random() * (maxNumber - minNumber) + minNumber).toFixed(numbersAfterComma));
+  }
+  console.log('Ошибка. Пожалуйста, введите число от 0 и больше');
+  return -1;
 }
 
-getCoordinates(0.8, 63, 2);
+getRandomFloat(0.8, 63, 2);
