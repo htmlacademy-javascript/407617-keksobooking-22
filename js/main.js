cfe2553a41541e7a3a78710bf2815243ec2ffd3b
Alexander <mybig8@hotmail.com>
2021-02-06 22:07:29 +0500

    Понял как это работает!

--- js/main.js
+++ js/main.js
@@ -1,29 +1,22 @@
+'use strict';
+
+// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomInt = (min, max) => {
-  if (min >= max) {
-    console.log('Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального');
-    return -1;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max <= min || min < 0 || max < 0) {
+    throw 'Ошибка. Пожалуйста, число от 0 и больше. Минимальное число должно быть меньше максимального';
   }
-  if (min >= 0 && max >= 0) {
-    return Math.trunc(Math.random() * (max - min + 1) + min);
-  }
-  console.log('Ошибка. Пожалуйста, введите число от 0 и больше');
-  return -1;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-
-getRandomInt(2, 8);
+getRandomInt(0, 3);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-const getRandomFloat = (minNumber, maxNumber, numbersAfterComma) => {
-  if (minNumber >= maxNumber) {
-    console.log('Ошибка. Пожалуйста, введите другое значение. Минимальное число должно быть меньше максимального');
-    return -1;
+const getRandomFloat = (minNumb, maxNumb, simbolsAfterComma) => {
+  if (maxNumb <= minNumb || minNumb < 0 || maxNumb < 0) {
+    throw 'Ошибка. Пожалуйста, число от 0 и больше. Минимальное число должно быть меньше максимального';
   }
-  if (minNumber >= 0 && maxNumber >= 0) {
-    return Number((Math.random() * (maxNumber - minNumber) + minNumber).toFixed(numbersAfterComma));
-  }
-  console.log('Ошибка. Пожалуйста, введите число от 0 и больше');
-  return -1;
+  return (Math.random() * (minNumb - maxNumb) + maxNumb).toFixed(simbolsAfterComma);
 }
-
-getRandomFloat(0.8, 63, 2);
+getRandomFloat(0, 2, 3);
